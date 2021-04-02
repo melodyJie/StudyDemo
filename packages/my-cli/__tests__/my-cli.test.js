169233bc143d367f32c7b8da36fe65eda4424195
j.yangf <j.yangf@ctrip.com>
2021-04-02 14:26:51 +0800

    feat: init my-cli

---
+++ packages/my-cli/__tests__/my-cli.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const myCli = require('..');
+
+describe('my-cli', () => {
+    it('needs tests');
+});
---
+++ packages/my-cli/lib/my-cli.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = myCli;
+
+function myCli() {
+    // TODO
+}
